7ba52ad119068828f97ace6439e35fd497f77571
moemeylane <wangarimaureen668@gmail.com>
2024-07-02 05:17:42 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.ceil(Math.random() * 100);
+console.log(random);
+
+let num3 = 14;
+let num4 = 10;
+
+let mod = num3 % num4;
+console.log(mod);
+
+let numbers = [5, 10, 15, 20, 18];
+
+let max = Math.max(...numbers);
+console.log(max);
